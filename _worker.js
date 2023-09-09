2676c8affc1bcdf24b5be68baa519ed4616049d7
catmeowjiao <138079152+catmeowjiao@users.noreply.github.com>
2023-09-09 10:14:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,8 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        // 修改下方的example.com为自己的节点ip/域名
-        url.hostname="example.com";
+        url.hostname="gpt.darkcoder15.tk";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
